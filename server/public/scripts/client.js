2584d3ec1447b35d1a421694d6300d8f5b315367
Sabrina Ciaciura <ciaci0028@gmail.com>
2021-12-15 10:22:50 -0600

    getting and receiving data for /comments

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,40 @@
+/* 
+    This code is running on person's computer
+*/
+
 $(document).ready(onReady);
 
 function onReady() {
     console.log('Super ready');
+
+    $('button').on('click', onClick);
+
+    // TODO
+    // Write some code to get data
+    // from GET /comments endpoint
+    // and then render data to the DOM
+
+    // Make a network/HTTP/AJAX request
+    // As an argument, gets an object
+    // Request over the network to data warehouse
+    let ajaxOptions = {
+        method: 'GET',
+        url: '/comments',
+    };
     
+    $.ajax(ajaxOptions)
+        .then( (response) => {
+            console.log('ajax request complete', response);
+    });
+
+    console.log(`
+        made a network request, but no one hsa time to wait for that
+        `);
+    
+}
+
+function onClick() {
+    $('button').css(
+        'background', 'green'
+    )
 }
--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+/*
+    This code is running on data warehouse computer
+*/
+
 const express = require('express');
 
 // express is a function
@@ -13,10 +17,12 @@ app.use(express.static('server/public'));
 // Endpoint === method + url
 // http://localhost:5000/space-jams
 // Two arguments, request and response
+// How we share files
 app.get('/space-jams', (req, res) => {
     console.log('\'bout to get some space jams');
 
     // Send back a response
+    // Manually send back some HTML
     res.send(`
     <h1> Ready to space jam? </h1>
     <button>Jame Time</button>
@@ -24,10 +30,25 @@ app.get('/space-jams', (req, res) => {
 
 });
 
+// Create a get comments end point
+// See this at the path
+app.get('/comments', (req, res) => {
+    console.log('comments have been visited');
+
+    res.send([
+        {
+            author: 'Edan',
+            message: '1996 space James ftw',
+        },
+    ]);
+});
+
 
 // listen on port 5000
 const port = 5000;
 app.listen(port, () => {
+    // This function is called when the server is up 
+    // and running, usually only a console.log
     console.log(`I'm listening`);
 });
 
